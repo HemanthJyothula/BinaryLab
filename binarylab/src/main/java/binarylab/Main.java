9291f713ae6321e59558775625d324abcb8df98a
Hemanth | TenaciousX <54387033+HemanthJyothula@users.noreply.github.com>
2026-09-05 23:47:27 +0530

    Baseline Requirements and structure
    Created the folder structures and baseline requirement to make a small window popup using java FX

---
+++ binarylab/src/main/java/binarylab/Main.java
@@ -0,0 +1,20 @@
+package binarylab;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        primaryStage.setTitle("Binary Lab");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
